d2fe73904376ba1de54ee4801e6dedebcba05651
tiagofsilva <aeroboom@gmail.com>
2017-06-15 03:17:11 -0300

    Refactors some code on equalizer class

--- equalizer.rb
+++ equalizer.rb
@@ -1,17 +1,25 @@
-class Equalizer < Module
+module ValueObject
+	class Equalizer < Module
 
-	def initialize(*attrs)
-		@attrs = attrs
-		define_method :eql? do |other|
-			self.kind_of?(other.class) && all_attrs_match?(other)
+		def initialize(*attrs)
+			@attrs = attrs
+			define_methods
 		end
 
-		define_method :all_attrs_match? do |other|
-			puts ':attrs match!!'
-			attrs.all? { |attr| self.send(attr) == other.send(attr) }			
+	private
+
+		def define_methods
+			attrs = @attrs
+			define_method :eql? do |other|
+				self.kind_of?(other.class) && all_attrs_match?(other)
+			end
+
+			define_method :all_attrs_match? do |other|
+				attrs.all? { |attr| self.send(attr) == other.send(attr) }
+			end
+
+			private :all_attrs_match?
 		end
-		
-		private :all_attrs_match?
+
 	end
-
 end
--- point.rb
+++ point.rb
@@ -2,9 +2,8 @@ require_relative 'equalizer'
 
 class Point
 	attr_accessor :x, :y
-	
-	include Equalizer.new(:x, :y)
 
+	include ValueObject::Equalizer.new(:x, :y)
 
 	def initialize(x, y)
 		@x, @y = x, y
