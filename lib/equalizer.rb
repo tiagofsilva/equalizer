073363ada400f86e409286e212479069d8bb5844
tiagofsilva <aeroboom@gmail.com>
2017-06-16 02:50:20 -0300

    Moves equalizer class to lib folder and changes #eql? for #eqvl?

---
+++ lib/equalizer.rb
@@ -0,0 +1,23 @@
+class Equalizer < Module
+
+	def initialize(*attrs)
+		@attrs = attrs
+		define_methods
+	end
+
+private
+
+	def define_methods
+		attrs = @attrs
+		define_method :eqvl? do |other|
+			self.kind_of?(other.class) && all_attrs_match?(other)
+		end
+
+		define_method :all_attrs_match? do |other|
+			attrs.all? { |attr| self.send(attr) == other.send(attr) }
+		end
+
+		private :all_attrs_match?
+	end
+
+end
