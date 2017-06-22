db3417523d429c48dff4e5c11349a9c60f22826d
tiagofsilva <aeroboom@gmail.com>
2017-06-22 02:11:11 -0300

    Applies changes in .editorconfig.

--- lib/equivalence.rb
+++ lib/equivalence.rb
@@ -1,23 +1,23 @@
 class Equivalence < Module
 
-	def initialize(*attrs)
-		@attrs = attrs
-		define_methods
-	end
+  def initialize(*attrs)
+    @attrs = attrs
+    define_methods
+  end
 
 private
 
-	def define_methods
-		attrs = @attrs
-		define_method :eqvl? do |other|
-			self.kind_of?(other.class) && all_attrs_match?(other)
-		end
+  def define_methods
+    attrs = @attrs
+    define_method :eqvl? do |other|
+      self.kind_of?(other.class) && all_attrs_match?(other)
+    end
 
-		define_method :all_attrs_match? do |other|
-			attrs.all? { |attr| self.send(attr) == other.send(attr) }
-		end
+    define_method :all_attrs_match? do |other|
+      attrs.all? { |attr| self.send(attr) == other.send(attr) }
+    end
 
-		private :all_attrs_match?
-	end
+    private :all_attrs_match?
+  end
 
 end
--- spec/equivalence_spec.rb
+++ spec/equivalence_spec.rb
@@ -1,62 +1,62 @@
 require 'spec_helper'
 
 describe Equivalence do
-	
-	let(:klass) { Class.new }
-	let(:instance) { klass.new }
 
-	before do
-		klass.send(:include, subject)
-	end
+  let(:klass) { Class.new }
+  let(:instance) { klass.new }
 
-	it { should be_instance_of(described_class)}
+  before do
+    klass.send(:include, subject)
+  end
 
-	context 'when it has no keys' do
-		
-		subject { Equivalence.new }
+  it { should be_instance_of(described_class)}
 
-		describe '#eqvl?' do
-			it 'returns true for equivalent objects' do
-				expect(instance.eqvl?(instance.dup)).to be(true)
-			end
+  context 'when it has no keys' do
 
-			it 'returns false for distinct objects' do
-				expect(instance.eqvl?(double(:other))).to be(false)
-			end
-		end	
-	end
+    subject { Equivalence.new }
 
-	context 'when it has keys' do
-		
-		let(:keys) { [:x, :y, :z] }
-		let(:klass) do
-			Class.new do
-	      attr_reader :x, :y, :z
-	      private :x, :y, :z
+    describe '#eqvl?' do
+      it 'returns true for equivalent objects' do
+        expect(instance.eqvl?(instance.dup)).to be(true)
+      end
 
-	      def initialize(x, y, z)
-	        @x = x
-	        @y = y
-	        @z = z
-	      end
-	    end
-		end
+      it 'returns false for distinct objects' do
+        expect(instance.eqvl?(double(:other))).to be(false)
+      end
+    end
+  end
 
-		subject { Equivalence.new(*keys) }
-		let(:instance) { klass.new(1, 2, 3) }
+  context 'when it has keys' do
 
-		describe '#eqvl?' do
-			it 'returns true for equivalent objects' do
-				other_instance = klass.new(1, 2, 3)
+    let(:keys) { [:x, :y, :z] }
+    let(:klass) do
+      Class.new do
+        attr_reader :x, :y, :z
+        private :x, :y, :z
 
-				expect(instance.eqvl?(other_instance)).to be(true)
-			end
+        def initialize(x, y, z)
+          @x = x
+          @y = y
+          @z = z
+        end
+      end
+    end
 
-			it 'returns false for distinct objects' do
-				other_instance = klass.new(3, 2, 1)
-				expect(instance.eqvl?(other_instance)).to be(false)
-			end
-		end	
-	end
+    subject { Equivalence.new(*keys) }
+    let(:instance) { klass.new(1, 2, 3) }
+
+    describe '#eqvl?' do
+      it 'returns true for equivalent objects' do
+        other_instance = klass.new(1, 2, 3)
+
+        expect(instance.eqvl?(other_instance)).to be(true)
+      end
+
+      it 'returns false for distinct objects' do
+        other_instance = klass.new(3, 2, 1)
+        expect(instance.eqvl?(other_instance)).to be(false)
+      end
+    end
+  end
 
 end
